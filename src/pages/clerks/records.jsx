33ddf36c4002f9ab08bdd99c38ba828aa5f3bb19
wisejoel77-creator <wisejoel77@gmail.com>
2026-08-25 12:21:50 +0300

    Add function handlesubmit and records to clerks/records.jsx file

---
+++ src/pages/clerks/records.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import ErrorMessage from "../../components/errorMessage";
+
+function Records() {
+  const [formData, setFormData] = useState({
+    product: "", quantity: "",
+    paymentStatus: "unpaid",
+    buyingPrice: "",
+    sellingPrice: "", spoilt: "",
+  });
+
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setFormData((prev) => ({ ...prev,
+      [name]: value,
+    }));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (!formData.product || !formData.quantity) {
+      setError("Product name and quantity are required.");
+      setSuccess("");
+      return;
+    }
+
+    setError("");
+    setSuccess("Inventory record saved successfully.");
+    console.log(formData);
+
+    setFormData({product: "", quantity: "",
+      paymentStatus: "unpaid", buyingPrice: "",
+      sellingPrice: "", spoilt: "", });}
+
+ 
