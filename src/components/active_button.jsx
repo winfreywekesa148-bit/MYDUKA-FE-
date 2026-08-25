abedcdc66fff95c09089f285c4d063351f92136c
cynthia <njugunacynthia32@gmail.com>
2026-08-25 03:06:02 +0300

    add active button.jsx

--- src/components/active_button.jsx
+++ src/components/active_button.jsx
@@ -1,1 +1,11 @@
+import React from "react";
 
+function ActiveButton({ active }) {
+  return (
+    <button type="button">
+      {active ? "Active" : "Inactive"}
+    </button>
+  );
+}
+
+export default ActiveButton;
