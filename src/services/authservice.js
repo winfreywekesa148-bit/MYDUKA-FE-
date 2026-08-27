b3462dea0a8cfd623c224d314aa637cb1f4f4696
Winkesa <winfreywekesa148@gmail.com>
2026-08-27 15:27:47 +0300

    feat: authorization services

---
+++ src/services/authservice.js
@@ -0,0 +1,67 @@
+import { apiRequest } from "./api";
+
+// Login
+export function login(email, password) {
+
+    return apiRequest("/auth/login", {
+
+        method: "POST",
+
+        body: JSON.stringify({
+            email,
+            password
+        })
+
+    });
+
+}
+
+
+// Merchant Registration
+export function registerMerchant(userData) {
+
+    return apiRequest("/auth/register-merchant", {
+
+        method: "POST",
+
+        body: JSON.stringify(userData)
+
+    });
+
+}
+
+
+// Admin Registration (with access code)
+export function registerAdmin(userData) {
+
+    return apiRequest("/auth/register-admin", {
+
+        method: "POST",
+
+        body: JSON.stringify(userData)
+
+    });
+
+}
+
+
+// Clerk Registration (with access code)
+export function registerClerk(userData) {
+
+    return apiRequest("/auth/register-clerk", {
+
+        method: "POST",
+
+        body: JSON.stringify(userData)
+
+    });
+
+}
+
+// Logout
+export function logout() {
+
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+
+}
