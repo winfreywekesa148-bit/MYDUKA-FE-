87844e1aea02e20739506267bac99d283eb5e34a
Winkesa <winfreywekesa148@gmail.com>
2026-08-27 14:27:47 +0300

    feat: Add Clerk routes to App component
    Added routes for Clerk Dashboard and related pages.

--- App.jsx
+++ App.jsx
@@ -5,16 +5,33 @@ import MerchantDashboard from "./src/pages/merchants/merchantdashboard";
 import MerchantAdmin from "./src/pages/merchants/admin";
 import MerchantStores from "./src/pages/merchants/stores";
 import MerchantPayment from "./src/pages/merchants/payment";
+import ClerkDashboard from "./pages/clerks/clerkdashboard";
+import Records from "./pages/clerks/records";
+import SupplyReq from "./pages/clerks/supplyreq";
+import Inventory from "./pages/clerks/inventory";
+import EditRecord from "./pages/clerks/editrecord";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/merchant/dashboard" replace />} />
 
       <Route path="/merchant/dashboard" element={<MerchantDashboard />} />
       <Route path="/merchant/admin" element={<MerchantAdmin />} />
       <Route path="/merchant/stores" element={<MerchantStores />} />
       <Route path="/merchant/payments" element={<MerchantPayment />} />
+      
+         {/* Clerk Dashboard */}
+      <Route path="/clerk" element={<ClerkDashboard />} />
+      <Route path="/clerk/dashboard" element={<ClerkDashboard />} />
+
+      {/* Clerk Pages */}
+      <Route path="/clerk/records" element={<Records />} />
+      <Route path="/clerk/supplyreq" element={<SupplyReq />} />
+      <Route path="/clerk/inventory" element={<Inventory />} />
+
+      {/* Edit Record */}
+      <Route path="/clerk/edit/:recordId" element={<EditRecord />} />
+      
     </Routes>
   );
 }
