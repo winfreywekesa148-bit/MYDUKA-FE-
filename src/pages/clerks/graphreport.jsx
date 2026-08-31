47e5b6f5b85d1c00a6b21cd9e34d4cda641f6130
Winkesa <winfreywekesa148@gmail.com>
2026-08-31 11:04:35 +0300

    feat: Add Sidebar and new Chart component to GraphReport

--- src/pages/clerks/graphreport.jsx
+++ src/pages/clerks/graphreport.jsx
@@ -1,12 +1,16 @@
 import ProductPieChart from "../charts/piechart";
 import SalesChart from "../charts/barchart";
+import Sidebar from "../../components/sidebar";
+import Chart from "../charts/barchart2";
 
 function GraphReport() {
   return (
     <div>
+      <Sidebar />
       <h1>Graph Report</h1>
       <ProductPieChart />
       <SalesChart />
+      <Chart />
     </div>
   );
 }  
